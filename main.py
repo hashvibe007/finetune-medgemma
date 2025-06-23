45fb18613b55e7064f637575083b0a72d9ae1f82
vibi007 <vibhashkumarorama@gmail.com>
2025-06-23 19:09:54 +0530

    bug fix - validationset

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from peft import LoraConfig
 # Preparing datasets for fine-tuning - task is to predict the medicines prescribed by the doctor in the given image
 
 data = load_dataset(
-    "chaithanyakota/100-handwritten-medical-records",split="train",
+    "chaithanyakota/100-handwritten-medical-records",
+    split="train",
 )
 
 # Define proportions for train and validation splits
@@ -29,9 +30,10 @@ data["validation"] = data.pop("test")
 # Display dataset details
 print(data)
 
-print(data['train'][0]['medicines'])
+print(data["train"][0]["medicines"])
 
 PROMPT = "What are the medicines prescribed by the doctor in the given image?"
+
 
 def format_data(example: dict[str, any]) -> dict[str, any]:
     example["messages"] = [
@@ -59,6 +61,7 @@ def format_data(example: dict[str, any]) -> dict[str, any]:
     ]
     return example
 
+
 # Apply the formatting to the dataset
 formatted_data = data.map(format_data)
 
@@ -68,12 +71,13 @@ print(formatted_data["train"][0]["messages"])
 # Fine-tuning the model - we will finetune google/medgemma-4b-it multimodal model
 
 
-
 model_id = "unsloth/medgemma-4b-it"
 
 # Check if GPU supports bfloat16
 if torch.cuda.get_device_capability()[0] < 8:
-    raise ValueError("GPU does not support bfloat16, please use a GPU that supports bfloat16.")
+    raise ValueError(
+        "GPU does not support bfloat16, please use a GPU that supports bfloat16."
+    )
 
 model_kwargs = dict(
     attn_implementation="eager",
@@ -90,7 +94,6 @@ processor.tokenizer.padding_side = "right"
 # setting up the model for training
 
 
-
 peft_config = LoraConfig(
     lora_alpha=16,
     lora_dropout=0.05,
@@ -105,9 +108,10 @@ peft_config = LoraConfig(
 )
 
 
-# To handle both image and text inputs during training, 
+# To handle both image and text inputs during training,
 # we define a custom collation function. This function processes the dataset examples into a format suitable for the model,
 # including tokenizing text and preparing image data.
+
 
 def collate_fn(examples: list[dict[str, any]]):
     texts = []
@@ -142,40 +146,37 @@ def collate_fn(examples: list[dict[str, any]]):
     return batch
 
 
-
-
 args = SFTConfig(
-    output_dir="medgemma-prescription",            
-    num_train_epochs=1,                       
-    per_device_train_batch_size=8,                           
-    per_device_eval_batch_size=8,                            
-    gradient_accumulation_steps=8,                           
-    gradient_checkpointing=True,                             
-    optim="adamw_torch_fused",                               
-    logging_steps=0.1,                                        
-    save_strategy="epoch",                                   
-    eval_strategy="steps",                                   
-    eval_steps=0.1,                                           
-    learning_rate=2e-4,                             
-    bf16=True,                                               
-    max_grad_norm=0.3,                                       
-    warmup_ratio=0.03,                                       
-    lr_scheduler_type="linear",                              
-    push_to_hub=True,                                        
+    output_dir="medgemma-prescription",
+    num_train_epochs=1,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    gradient_accumulation_steps=8,
+    gradient_checkpointing=True,
+    optim="adamw_torch_fused",
+    logging_steps=0.1,
+    save_strategy="epoch",
+    eval_strategy="steps",
+    eval_steps=0.1,
+    learning_rate=2e-4,
+    bf16=True,
+    max_grad_norm=0.3,
+    warmup_ratio=0.03,
+    lr_scheduler_type="linear",
+    push_to_hub=True,
     report_to="none",
-    gradient_checkpointing_kwargs={"use_reentrant": False},  
-    dataset_kwargs={"skip_prepare_dataset": True},           
-    remove_unused_columns = False,                           
-    label_names=["labels"],                                  
+    gradient_checkpointing_kwargs={"use_reentrant": False},
+    dataset_kwargs={"skip_prepare_dataset": True},
+    remove_unused_columns=False,
+    label_names=["labels"],
 )
-
 
 
 trainer = SFTTrainer(
     model=model,
     args=args,
     train_dataset=formatted_data["train"],
-    eval_dataset=formatted_data["validation"].shuffle().select(range(50)), 
+    eval_dataset=formatted_data["validation"].shuffle().select(range(10)),
     peft_config=peft_config,
     processing_class=processor,
     data_collator=collate_fn,
@@ -188,4 +189,3 @@ trainer.train()
 # Saving the model
 
 trainer.save_model()
-
